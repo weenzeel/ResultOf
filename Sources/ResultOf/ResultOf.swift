cab1678fb70c954a42191016f3718cd6d27a034e
Erik Lundgren <erik.karl.wenzel@gmail.com>
2020-07-21 15:54:15 +0200

    Add public access modifier for "resultOf" function

--- Sources/ResultOf/ResultOf.swift
+++ Sources/ResultOf/ResultOf.swift
@@ -6,6 +6,6 @@ A helper function for calling and returning the value of a closure in an ideomat
 Useful e.g. to hide temporary configuration logic, when only the final value is of interest to the system.
 */
 
-func resultOf<T>(_ operation: () -> T) -> T {
+public func resultOf<T>(_ operation: () -> T) -> T {
     return operation()
 }
